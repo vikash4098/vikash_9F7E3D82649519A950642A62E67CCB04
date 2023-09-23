57dc268220539e6c6150c82ef5ac211cb4fde3a1
vikash4098 <145836039+vikash4098@users.noreply.github.com>
2023-09-23 14:21:24 +0530

    Add files via upload

---
+++ Unit-1/ex 1.py
@@ -0,0 +1,29 @@
+num1 = int(input("Enter the frist number:"))
+num2 = int(input("Enter the second number:"))
+
+sum = num1 + num2 # an arithmetic expression 
+
+print("sum of two number is",sum)
+print("Relation Expression")
+
+#python program to find greater between three numbers.
+
+num1 = int(input("Enter the frist number:"))
+num2 = int(input("Enter the second number:"))
+num3 = int(input("Enter the thrid number:"))
+
+if num1>num2> and num1>num3
+print(num1,'is a grater number amng the three numbers.")
+
+elif num2>num1 and num2>num3
+print(num2,'is a greater number among the tree numbers.")
+
+else 
+print(number3,'is a greater number among the three numbers.")
+
+print(" logical expression")
+p = 20
+q = 15
+r = 30
+s = 20
+result = p>q and r>s
---
+++ Unit-1/ex 2.py
@@ -0,0 +1,23 @@
+a = 9 
+b = 6 
+
+# addition 
+print('sum:', a+b)
+
+# subtraction
+print('subtractin:', a-b)
+
+# multipilication 
+print('multipilication:', a*b)
+
+# division
+print('division:', a/b)
+
+# floor division
+print('floor division:', a//b)
+
+# module
+print('module:', a%b)
+
+# a to the power b 
+print ('power:', a**b)
